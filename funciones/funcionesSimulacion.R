cf606e4ce4a001d79c622d7cf94e53c322c1afac
arlingva <arling.vazquez@gmail.com>
2021-02-05 18:19:03 -0700

    varianza muestreo estratificado

--- funciones/funcionesSimulacion.R
+++ funciones/funcionesSimulacion.R
@@ -132,6 +132,60 @@ funcionSimulaEstrProp <- function(df, n, M, df.distritos){
   return(resultado)  
 }
 
+funcionSimulaEstrVar <- function(df, n, M, df.distritos){
+  distritos <- df.distritos$distrito_local
+  N.dist <- df.distritos$N.dist
+  n.dist <- df.distritos$n.dist
+  L <- length(distritos)
+  
+  # Crea una matriz vacía para el resultado
+  resultado <- data.frame()
+  
+  # aquí se guardarán el estimador y varianza de cada partido, de cada muestra
+  estPRI<-rep(0, M)
+  estPAN<-rep(0, M)
+  varPRI<-rep(0, M)
+  varPAN<-rep(0, M)
+  
+  for(i in 1:M){
+    muestra <- c() 
+    # en este ciclo se selecciona la muestra dentro de cada estrato
+    for(j in 1: L){
+      df %>%
+        filter(distrito_local == distritos[j]) %>%
+        select(seccion_casilla) -> df.muestra
+      muestra <- c(muestra, sample(df.muestra$seccion_casilla, df.distritos$n.dist[j]))
+    } 
+    
+    # Se calcula el porcentaje de votos de cada estrato para cada muestra
+    df.temporal <- df %>%
+      filter(seccion_casilla %in% muestra) %>%
+      group_by(distrito_local) %>% 
+      summarise(PRI = 100 * sum(total_coalicion)/sum(votacion_total_emitida),
+                PAN = 100 * sum(pan) / sum(votacion_total_emitida)) 
+    
+    # para cada muestra se calcula el estimador y la varianza
+    estPRI[i] = N.dist %*% df.temporal$PRI / N
+    estPAN[i] = N.dist %*% df.temporal$PAN / N
+    varPRI[i] = sum((1-n.dist/N.dist) * 
+                      (N.dist/sum(N.dist))^2 *
+                      (estPRI*(100-estPRI)) / (n.dist-1))
+    varPAN[i] = sum((1-n.dist/N.dist) * 
+                      (N.dist/sum(N.dist))^2 *
+                      (estPAN*(100-estPAN)) / (n.dist-1))
+    
+  }
+  
+  resultado <- cbind(estPRI, estPAN, varPRI, varPAN)
+  colnames(resultado) <- c("estPRI", "estPAN", "varPRI", "varPAN")
+  resultado <- resultado %>%
+    as.data.frame() %>%
+    mutate(dif.PRI = estPRI - resultadoReal$PRI,
+           dif.PAN = estPAN - resultadoReal$PAN)
+  return(resultado)  
+}
+
+
 funcionSimulaEstr02 <- function(df, n, M, df.distritos){
   distritos <- df.distritos$distrito_tipo
   L <- length(distritos)
